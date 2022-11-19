102f6b315126a39533d4c96f6857a3ae310f6d88
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-11-19 17:55:43 +0200

    terminat a 2-ua pagina?

--- src/Second Page/SecondPage.jsx
+++ src/Second Page/SecondPage.jsx
@@ -5,23 +5,18 @@ import background from "../background.png";
 
 function SecondPage() {
   return (
-    <section
-      className="container"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <div>
-        <h4 className="secondpagetext">
-          It is essential for any business <br /> to have a professional online{" "}
-          <br /> presence. <br />
-        </h4>
+    <section style={{ backgroundImage: `url(${background})` }}>
+      <h4>Web Design</h4>
 
-        <h5>
-          Using thelatest technologies, <br /> I will create a fully responsive{" "}
+      <div className="second-container">
+        <h5 className="secondpagetext">
+          It is essential for any business to <br /> have a professional online{" "}
+          <br /> presence. <br />
+          Using thelatest technologies, I will
           <br />
+          create a fully responsive <br />
           website.
         </h5>
-      </div>
-      <div>
         <img src={secondslicepicture} className="secondpicture" alt="" />
       </div>
     </section>
@@ -29,30 +24,3 @@ function SecondPage() {
 }
 
 export default SecondPage;
-/*
-
-
-function SecondPage() {
-  return (
-    <section
-      className="container"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <div>
-        <h3 className="secondpagetext">
-          It is essential for <br /> any business to have <br /> a professional
-          online <br /> presence. Using the <br /> latest technologies, <br /> I
-          will create a fully responsive website.
-        </h3>
-      </div>
-      <div>
-    <img src={secondslicepicture} className="secondpicture" alt="" />
-      </div>
-    </section>
-  );
-}
-
-export default SecondPage;
-
-
-*/
