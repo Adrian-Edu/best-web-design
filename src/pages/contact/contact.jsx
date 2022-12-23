44d53c141c4ff45861ad74b9f8067aa616c4ae7b
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-12-23 23:22:05 +0200

    un pic si termin inputul

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -3,19 +3,24 @@ import "./contact.css";
 
 function Contact(props) {
   const [message, setMessage] = useState({
-    firstname: "",
-    lastname: "",
+    name: "",
+    surname: "",
     email: "",
     question: "",
   });
 
+  const [errorName, setErrorName] = useState(false);
+  const [errorSurname, setErrorSurname] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorQuestion, setErrorQuestion] = useState(false);
   const [messageSubmit, setMessageSubmit] = useState(false);
+  const [submit, setSubmit] = useState("");
 
   const handleNameInput = (e) => {
-    setMessage({ ...message, firstname: e.target.value });
+    setMessage({ ...message, name: e.target.value });
   };
 
-  const handleLastNameInput = (e) => {
+  const handleSurnameInput = (e) => {
     setMessage({ ...message, lastname: e.target.value });
   };
 
@@ -28,13 +33,56 @@ function Contact(props) {
   };
 
   const handleSubmit = (e) => {
-    e.prevent.default();
+    e.preventDefault();
     setMessageSubmit(true);
+
+    setSubmit(
+      message.name +
+        " " +
+        message.surname +
+        " " +
+        message.email +
+        " " +
+        message.question
+    );
+
+    if (
+      message.name === "" &&
+      message.surname === "" &&
+      message.email === "" &&
+      message.question === ""
+    ) {
+      return (
+        setErrorName(true) +
+        setErrorSurname(true) +
+        setErrorQuestion(true) +
+        setErrorEmail(true)
+      );
+    } else if (message.name === "") {
+      return setErrorName(true);
+    } else if (message.surname === "") {
+      return setErrorSurname(true);
+    } else if (message.email === "") {
+      return setErrorEmail(true);
+    } else if (message.question === "") {
+      return setErrorQuestion(true);
+    } else {
+      setMessageSubmit(true) && setErrorName(false);
+      setErrorSurname(false);
+      setErrorEmail(false);
+      setErrorQuestion(false);
+    }
+
+    setErrorName(false);
+    setErrorSurname(false);
+    setErrorEmail(false);
+    setErrorQuestion(false);
   };
 
   return (
     <section>
-      <div
+      <form
+        onSubmit={handleSubmit}
         className="container-contact"
         style={{ backgroundColor: `${props.background}` }}
       >
@@ -45,7 +93,7 @@ function Contact(props) {
           for="fname"
           onChange={handleNameInput}
         >
-          First Name
+          Name
         </label>
         <input
           type="text"
@@ -53,16 +101,26 @@ function Contact(props) {
           name="firstname"
           placeholder="Your name is..."
         ></input>
+        {errorName ? (
+          <div>
+            <span>Please fill in the name field!</span>
+          </div>
+        ) : null}
         <label style={{ color: `${props.textColor}` }} for="lname">
-          Last Name
+          Surname
         </label>
         <input
           type="text"
           id="lname"
           name="lastname"
-          placeholder="Your last name is..."
-          onChange={handleLastNameInput}
+          placeholder="Your surname is..."
+          onChange={handleSurnameInput}
         ></input>
+        {errorSurname ? (
+          <div>
+            <span>Please fill in the surname field! </span>{" "}
+          </div>
+        ) : null}
         <label style={{ color: `${props.textColor}` }} for="email">
           Email
         </label>
@@ -73,14 +131,11 @@ function Contact(props) {
           placeholder="Your email is..."
           onChange={handleEmailInput}
         ></input>
-        <label style={{ color: `${props.textColor}` }} for="subject">
-          Topic
-        </label>
-        <select id="country" name="country">
-          <option value="customer">Customer service</option>
-          <option value="requisition">Requisition for offers</option>
-          <option value="other">Other</option>
-        </select>
+        {errorEmail ? (
+          <div>
+            <span>Please fill your email!</span>{" "}
+          </div>
+        ) : null}
         <label style={{ color: `${props.textColor}` }} for="fname">
           Question
         </label>
@@ -92,10 +147,21 @@ function Contact(props) {
           placeholder="Message..."
           onChange={handleQuestionInput}
         ></input>
+        {errorQuestion ? (
+          <div>
+            <span>Please fill the question!</span>{" "}
+          </div>
+        ) : null}
         <input type="submit" value="Submit"></input>
         {messageSubmit ? (
           <div style={{ color: `${props.textColor}` }} for="fname">
             Your message has been successfully sent!
+          </div>
+        ) : null}
+
+        {messageSubmit ? (
+          <div>
+            <p>{submit}</p>
           </div>
         ) : null}
 
@@ -106,7 +172,7 @@ function Contact(props) {
         <p className="contact-p" style={{ color: `${props.textColor}` }}>
           Contact us by email: <br /> adrian_edu@yahoo.com
         </p>
-      </div>
+      </form>
     </section>
   );
 }
