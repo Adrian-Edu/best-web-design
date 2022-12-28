7f34219d971055b6edfb143e47cd5f3efe3edad8
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-12-28 23:56:16 +0200

    am terminat pagina de about?

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./AboutUs.css";
+import about from "../../assets/about.png";
 
 function AboutUs(props) {
   return (
     <div
-      className="page-height"
+      className="page-height-about"
       style={{ backgroundColor: `${props.background}` }}
     >
-      <h4>About us</h4>
-      <p>Story</p>
-      <p>Values</p>
+      <p className="header-about">About us</p>
+      <img src={about} alt="" className="about" />
+      <p className="about-us-value" style={{ color: `${props.textColor}` }}>
+        FOLLOW YOUR PASSION AND YOU'LL NEVER WORK A DAY!
+      </p>
+      <p className="about-us-value" style={{ color: `${props.textColor}` }}>
+        I think this quote is the most meaningful value in our lives and the one
+        that makes a big difference between us and others. <br /> My passion for
+        programming will make your website look outstanding, <br /> whether it's
+        your personal or professional website.
+      </p>
     </div>
   );
 }
