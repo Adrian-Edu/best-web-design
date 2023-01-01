ab9ca1c0cdf51ba26f52f3b77915c0dc5a3616b1
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-01-01 11:37:54 +0200

    bravo - am pus un svg la 404 si merge :D!

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BackToTopButton from "../../components/back-top-button/BackToTopButton";
 import "./NotFound.css";
+import error404svg from "../../assets/Error404.gif";
 
 function NotFound(props) {
   const [time, setTime] = useState(5);
@@ -21,25 +22,32 @@ function NotFound(props) {
   RedirectExample();
 
   return (
-    <div
+    <section
       className="page-height"
       style={{
         backgroundColor: `${props.background}`,
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <h4 style={{ color: `${props.textColor}` }}>
-        The page you requested DOESN'T EXIST! <br /> The page will automatically
-        <br />
-        redirect in {time} seconds.
-      </h4>
+      <img className="error404pic" src={error404svg} alt="error404svg" />
+      <p className="error-p" style={{ color: `${props.textColor}` }}>
+        The page will automatically redirect in {time} seconds.
+      </p>
+      <a
+        style={{ color: `${props.textColor}` }}
+        className="author"
+        href="https://storyset.com/internet"
+      >
+        Internet illustrations by Storyset
+      </a>
       <BackToTopButton
         background={props.background}
         textColor={props.textColor}
       />
-    </div>
+    </section>
   );
 }
 
