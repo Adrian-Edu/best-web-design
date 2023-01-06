c83272f85a050671f614f9c24e3a9d27660da747
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-01-06 21:27:43 +0200

    normal

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import "./Home.css";
 import pozamea from "../../assets/pozamea.png";
 import { Link } from "react-router-dom";
 import BackToTopButton from "../../components/back-top-button/BackToTopButton";
-import Fade from "react-reveal/Fade";
 
 function Home(props) {
   return (
@@ -13,27 +12,24 @@ function Home(props) {
         style={{ backgroundColor: `${props.background}` }}
       >
         <div>
-          <Fade left>
-            <h1 style={{ color: `${props.textColor}` }}>
-              My name is Adrian Edu,
-            </h1>
+          <h1 style={{ color: `${props.textColor}` }}>
+            My name is Adrian Edu,
+          </h1>
 
-            <h2 style={{ color: `${props.textColor}` }}>
-              I am a Front-End <br /> Developer
-            </h2>
+          <h2 style={{ color: `${props.textColor}` }}>
+            I am a Front-End <br /> Developer
+          </h2>
 
-            <h3 style={{ color: `${props.textColor}` }}>
-              <Link className="remove-format-footer-home" to="/Contact">
-                <p style={{ color: `${props.textColor}` }}>Contact me !</p>
-              </Link>
-            </h3>
-          </Fade>
+          <h3 style={{ color: `${props.textColor}` }}>
+            <Link className="remove-format-footer-home" to="/Contact">
+              <p style={{ color: `${props.textColor}` }}>Contact me !</p>
+            </Link>
+          </h3>
         </div>
-        <Fade right>
-          <div>
-            <img src={pozamea} className="profilepic" alt="" />
-          </div>
-        </Fade>
+
+        <div>
+          <img src={pozamea} className="profilepic" alt="" />
+        </div>
       </div>
       <BackToTopButton
         background={props.background}
