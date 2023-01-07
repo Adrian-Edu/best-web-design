2ed93d465dc791da1ee0ba67d70de39617a227e7
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-01-07 23:22:48 +0200

    progresez

--- src/pages/slide-protfolio/SlidePortfolio.jsx
+++ src/pages/slide-protfolio/SlidePortfolio.jsx
@@ -4,33 +4,51 @@ import product from "../../assets/product.png";
 import todo from "../../assets/todo.png";
 import lucky from "../../assets/lucky.png";
 import robo from "../../assets/robo.png";
-import { useEffect } from "react";
 
 function SlidePortfolio(props) {
   const [buttonColor, setButtonColor] = useState("#ffffff");
 
   const portfolioData = [
     {
-      nume: "Product website",
+      id: 1,
+      name: "Product website",
       image: product,
       url: "https://lading-page-12.adrianedu.repl.co/",
     },
     {
-      nume: "Todo App",
+      id: 2,
+      name: "Todo App",
       image: todo,
       url: "https://todo-app-dun-phi.vercel.app/",
     },
     {
-      nume: "The lucky dice game",
+      id: 3,
+      name: "The lucky dice game",
       image: lucky,
       url: "https://lucky-dice-alpha.vercel.app/",
     },
     {
-      nume: "Robot joketeller",
+      id: 4,
+      name: "Robot joketeller",
       image: robo,
       url: "https://robo-joke-teller.adrianedu.repl.co/",
     },
   ];
+
+  const [currentPortfolio, setCurrentPortfolio] = useState([
+    portfolioData[0],
+    portfolioData[1],
+    portfolioData[2],
+    portfolioData[3],
+  ]);
+
+  const changeProjectRight = () => {
+    setCurrentPortfolio((prevState) => prevState.currentPortfolio + 1);
+  };
+
+  const changeProjectLeft = () => {
+    setCurrentPortfolio();
+  };
 
   return (
     <section
@@ -40,6 +58,39 @@ function SlidePortfolio(props) {
       <div>
         <p className="header-slide">Portfolio</p>
       </div>
+      {[currentPortfolio[0]].map((project) => {
+        return (
+          <div key={portfolioData.name}>
+            <p className="h10" style={{ color: `${props.textColor}` }}>
+              {project.name}
+            </p>
+            <div className="slide-flex">
+              <button
+                onClick={changeProjectLeft}
+                onMouseEnter={() => setButtonColor("rgba(0, 0, 0, 1)")}
+                onMouseLeave={() => setButtonColor("#ffffff")}
+                style={{ backgroundColor: `${buttonColor}` }}
+                className="button-slide"
+              >
+                ←
+              </button>
+              <img src={project.image} className="slide-image" alt="" />
+              <button
+                onClick={changeProjectRight}
+                onMouseEnter={() => setButtonColor("rgba(0, 0, 0, 1)")}
+                onMouseLeave={() => setButtonColor("#ffffff")}
+                style={{ backgroundColor: `${buttonColor}` }}
+                className="button-slide"
+              >
+                →
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <p style={{ color: `${props.textColor}` }}>
+        See the project by clicking on the image!
+      </p>
     </section>
   );
 }
