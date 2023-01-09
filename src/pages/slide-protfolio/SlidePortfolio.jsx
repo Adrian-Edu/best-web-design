5f709cf1af1a4f7d5eca4d3b3b3881318a175ab0
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-01-09 09:44:33 +0200

    modificari

--- src/pages/slide-protfolio/SlidePortfolio.jsx
+++ src/pages/slide-protfolio/SlidePortfolio.jsx
@@ -69,11 +69,6 @@ function SlidePortfolio(props) {
       <div className="slide-flex">
         <button
           onClick={changeProjectPrevious}
-<<<<<<< HEAD
-=======
-          onMouseEnter={() => setButtonColor("")}
-          onMouseLeave={() => setButtonColor(" ")}
->>>>>>> 33831da6c8369b397b57a15f7e9309903025e05a
           style={{ backgroundColor: `${buttonColor}` }}
           className="button-slide"
         >
@@ -95,11 +90,6 @@ function SlidePortfolio(props) {
 
         <button
           onClick={changeProjectTowards}
-<<<<<<< HEAD
-=======
-          onMouseEnter={() => setButtonColor(" ")}
-          onMouseLeave={() => setButtonColor(" ")}
->>>>>>> 33831da6c8369b397b57a15f7e9309903025e05a
           style={{ backgroundColor: `${buttonColor}` }}
           className="button-slide"
         >
