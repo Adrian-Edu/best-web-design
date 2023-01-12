550ebb0d1b72c6a0fc1b820629b9b01aae8e3b78
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-01-12 22:40:53 +0200

    progresez

--- src/index.js
+++ src/index.js
@@ -4,6 +4,51 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// STORE-UL LOCUL UNDE TINEM DATELE
+
+// ACTIONS
+
+const changeBackgroundToWhite = () => {
+  return {
+    type: { CHANGETOWHITE: "#d0efff" },
+  };
+};
+
+const changeBackgroundToDark = () => {
+  return {
+    type: { CHANGETODARK: "#151B54" },
+  };
+};
+
+const changeTextColorToWhite = () => {
+  return {
+    type: { CHANGETOWHITE: "#FFFFFF" },
+  };
+};
+
+const changeTextColorToDark = () => {
+  return {
+    type: { CHANGETDARK: "#0e1111" },
+  };
+};
+
+// REDUCER
+
+const chooseBackgroundColor = (standard = "#151B54", action) => {
+  switch (action.type) {
+    case "changeBackgroundToWhite":
+      return "#d0efff";
+    case "changeBackgroundToDark":
+      return "#151B54";
+    default:
+      return standard;
+  }
+};
+
+// DISPLAY IN CONSOLE
+
+// DISPACH
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
