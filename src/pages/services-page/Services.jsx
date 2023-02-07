089c0e3c3c4732dcc098a35d81e75ef10d54b40d
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-02-07 09:22:38 +0200

    modificari

--- src/pages/services-page/Services.jsx
+++ src/pages/services-page/Services.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Services.css";
 import BackToTopButton from "../../components/back-top-button/BackToTopButton";
-import desktop from "../../assets/desktop.avif";
+import desktopul from "../../assets/dekstop.avif";
 import oldnew from "../../assets/oldvsnew.avif";
 import "animate.css";
 import Typewriter from "typewriter-effect";
@@ -28,7 +28,7 @@ function Services(props) {
           <p className="h7" style={{ color: `${props.textColor}` }}>
             Responsive website <br /> design
           </p>
-          <img src={desktop} className="desktop-image" alt="dekstop img" />
+          <img src={desktopul} className="desktop-image" alt="dekstop img" />
         </div>
         <div className="second2-fourtpage  animate__animated animate__backInRight">
           <p className="h7" style={{ color: `${props.textColor}` }}>
